87a87646ee6612970a2abd8ec4d5d06f137aae78
David Nuernberger <nuernberger.david@gmail.com>
2012-10-26 16:48:25 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ubiquitous::Application.config.secret_token = '128b3966b2d0b95769da321f54535683d4804dbed5ca377d55976aa2800501fb72bfe079a3f1b243e5465c9988890901460a9e80d44b79fffc7c3d11269c92e6'
